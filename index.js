5d4b1670bb798e4b50741fa75cba671aa5c9c10e
Gabriel-developments <GabrielTheDeveloperOFC@gmail.com>
2025-06-19 13:50:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDB = require('./db');
 const routes = require('./routes');
 
 connectDB();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use('/api', routes);
 
